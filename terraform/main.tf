d3a86de4073dcaa920dd07bcb0c53b5a11ef7b50
root <root@DESKTOP-SH1QMTE>
2025-12-31 18:45:39 +0530

    Add terraform folder as normal files

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source = "./networking"
+}
+
+module "alb" {
+  source         = "./alb"
+  vpc_id         = module.vpc.vpc_id
+  public_subnets = module.vpc.public_subnets
+#   project_name   = var.project_name
+#   environment    = var.environment
+}
